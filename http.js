cf301cd531f5ddb8ae81b3ccd0d32c879ecfc16f
Fabio Delgado Pereira <fabio.delgado2@gmail.com>
2018-04-24 22:42:30 -0300

    Implementação com metodo post

--- http.js
+++ http.js
@@ -2,32 +2,50 @@ var router = require('./router');
 
 var app = router(3412)
 
- var operadoras = [
-                {nome: "Oi", codigo: 14, categoria:"Celular", preco:2},
-                {nome: "Vivo", codigo: 15, categoria:"Celular", preco:1},
-                {nome: "TIM", codigo: 41, categoria:"Celular", preco:3},
-                {nome: "GVT", codigo: 25, categoria:"Fixo", preco:1},
-                {nome: "Embratel", codigo: 21, categoria:"Fixo", preco:2}
-            ];
- var contatos = [
-				{ nome: "Pedro", telefone: "99896575", data: new Date(),operadora: {nome: "Oi", codigo: 14, categoria:"Celular"} },
-                { nome: "Ana", telefone: "99456575" , data: new Date(),operadora: {nome: "Vivo", codigo: 15, categoria:"Celular"} },
-                { nome: "Maria", telefone: "99987575" , data: new Date(),operadora: {nome: "TIM", codigo: 41, categoria:"Celular"} }
-            
-            ];
-app.get('/operadoras',function(req,res){
-res.write(JSON.stringify(operadoras));	
-res.end();
-});	
+var operadoras = [
+    { nome: "Oi", codigo: 14, categoria: "Celular", preco: 2 },
+    { nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1 },
+    { nome: "TIM", codigo: 41, categoria: "Celular", preco: 3 },
+    { nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1 },
+    { nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2 }
+];
+var contatos = [
+    { nome: "Pedro", telefone: "99896575", data: new Date(), operadora: { nome: "Oi", codigo: 14, categoria: "Celular" } },
+    { nome: "Ana", telefone: "99456575", data: new Date(), operadora: { nome: "Vivo", codigo: 15, categoria: "Celular" } },
+    { nome: "Maria", telefone: "99987575", data: new Date(), operadora: { nome: "TIM", codigo: 41, categoria: "Celular" } }
 
-app.get('/contatos',function(req,res){
-res.write(JSON.stringify(contatos));	
-res.end();
-});				
-			
-app.post('/contatos',function(req,res){
-res.end();
-});	
-app.post('/operadoras',function(req,res){
-res.end();
-});	
+];
+app.interceptor(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.interceptor(function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    next();
+});
+
+app.get('/operadoras', function (req, res) {
+    res.write(JSON.stringify(operadoras));
+    res.end();
+});
+
+app.get('/contatos', function (req, res) {
+    res.write(JSON.stringify(contatos));
+    res.end();
+});
+
+app.post('/contatos', function (req, res) {
+    var contato = req.body;
+    contatos.push(JSON.parse(contato));
+    res.end();
+});
+app.post('/operadoras', function (req, res) {
+    res.end();
+});
+app.options('/contatos', function (req, res) {
+
+    res.end();
+  
+  });
--- router.js
+++ router.js
@@ -2,24 +2,71 @@ var http = require('http');
 
 
 var createRouter = function (port) {
-	var api={};
+	var api = {};
 	var routes = {};
-	var methods = ['GET','POST'];
-	
-methods.forEach(function(method){
-	routes[method] = {};
-	api[method.toLowerCase()]=function (path,fn){
-	routes[method][path] = fn;
-	};
+
+	var methods = ['GET', 'POST', 'OPTIONS'];
+	var interceptors = [];
+
+	methods.forEach(function (method) {
+		routes[method] = {};
+		api[method.toLowerCase()] = function (path, fn) {
+			routes[method][path] = fn;
+		};
 	});
-	
-		
-	http.createServer(function(req,res){
-		res.setHeader('Access-Control-Allow-Origin','*');
-	if(!routes[req.method][req.url]) return res.end();
-	routes[req.method][req.url](req,res);
+	api.interceptor = function (interceptor) {
+		interceptors.push(interceptor);
+	};
+
+	var executeInterceptors = function (number, req, res) {
+		var interceptor = interceptors[number];
+		if (!interceptor) return;
+		interceptor(req, res, function () {
+			executeInterceptors(++number, req, res);
+		});
+	};
+
+	var handleBody = function (req, res, next) {
+
+		var body = [];
+
+		req.on('data', function (chunk) {
+
+			body.push(chunk);
+
+		});
+
+		req.on('end', function () {
+
+			req.body = Buffer.concat(body).toString();
+
+			next();
+
+		});
+
+	};
+	http.createServer(function (req, res) {
+
+		console.log(req.url);
+
+		handleBody(req, res, function () {
+
+			executeInterceptors(0, req, res);
+
+			if (!routes[req.method][req.url]) {
+
+				res.statusCode = 404;
+
+				return res.end();
+
+			}
+
+			routes[req.method][req.url](req, res);
+
+		});
+
 	}).listen(port);
 
-return api
+	return api
 };
 module.exports = createRouter;
